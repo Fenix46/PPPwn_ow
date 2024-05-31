5e3cf7d56d59697194c3d23d72a67691f4c4742e
Emanuele Scarlata <scarlataemanuele@gmail.com>
2024-05-31 13:09:37 +0200

    install.sh: fix new repo name and add pppwn.bin call

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-mv -f ~/PPPwn_ow-main/offsets ~/
-mv -f ~/PPPwn_ow-main/run.sh ~/
-rm -r PPPwn_ow-main main
+mv -f ~/pppwn_owt-main/offsets ~/
+chmod +x pppwn
+mv -f ~/pppwn_owt-main/pppwn ~/usr/bin
+mv -f ~/pppwn_owt-main/run.sh ~/
+rm -r pppwn_owt-main main
 chmod +x run.sh
 echo "Installation completed!"
 exit 1
