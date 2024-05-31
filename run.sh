1e921ad8ecd4b1847b82da6b9d66e49a1285154d
Emanuele Scarlata <scarlataemanuele@gmail.com>
2024-05-31 13:04:04 +0200

    run.sh: for now lunch only 9.00 fw

--- run.sh
+++ run.sh
@@ -1,24 +1,3 @@
 #!/bin/sh
 
-INTERFACE=""
-FIRMWARE=""
-COUNTNUM="0"
-
-ip link set $INTERFACE down
-sleep 5
-ip link set $INTERFACE up
-
-while true; do
-    ret=$(pppwn --interface "$INTERFACE" --fw "$FIRMWARE" --stage1 "/root/offsets/stage1_$FIRMWARE.bin" --stage2 "/root/offsets/stage2_$FIRMWARE.bin" --auto-retry)
-    if [ "$ret" -ge 1 ]; then
-        echo "\n$ret\n" > /root/state.txt
-        echo -e "\nConsole PPPwned!\n" >> /root/state.txt
-        exit 0
-    else
-        COUNTNUM=$((COUNTNUM+1))
-        echo -e "\nFailed ($COUNTNUM) retrying...\n" > /root/state.txt
-        ip link set $INTERFACE down
-        sleep 5
-        ip link set $INTERFACE up
-    fi
-done
+pppwn --interface br-lan --fw 900 --stage1 "/root/offsets/stage1_900.bin" --stage2 "/root/offsets/stage2_900.bin" --auto-retry
